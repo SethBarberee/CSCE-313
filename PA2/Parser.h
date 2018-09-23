76b9c3ab7bd468b23505f46ad580ecb8d5ac8fc1
Seth Barberee <seth.barberee@gmail.com>
2018-09-23 14:27:55 -0500

    reversed recursion to act like a normal shell with pipes

--- PA2/Parser.h
+++ PA2/Parser.h
@@ -5,32 +5,47 @@ std::string parse_command(std::string command, int index){
     int new_index = index;
     std::string to_parse = command;
     std::string parsed = "";
-    std::string sub_parse = "";
+    std::string left_half = "";
+    std::string right_half = "";
     std::cout << "Command entered: " << to_parse << std::endl;
-    for(int i = new_index; i < to_parse.length(); i++){
+    // Iterate from right to left so the recursion will execute the left-most command first
+    for(int i = to_parse.length(); i >= new_index; i--){
         char to_parse_char = to_parse[i];
         switch(to_parse_char){
             case '&':
                         std::cout << "TODO implement forking" << std::endl;
                         break;
             case '|':
-                        // TODO Parse the other side
-                        sub_parse = to_parse.substr(i+1);
-                        std::cout << "Checking " << sub_parse << std::endl;
-                        parsed = parsed +  parse_command(sub_parse, i+1);
+                        left_half = to_parse.substr(0,i-1);
+                        right_half = to_parse.substr(i+2);
+                        std::cout << "Checking " << left_half << std::endl;
+                        // Parse the left half
+                        parse_command(left_half, 0);
+                        // Parse the right half
+                        parse_command(right_half,0);
                         break;
             case '>':
             case '<':
                         std::cout << "TODO implement redirection" << std::endl;
+                        left_half = to_parse.substr(0,i-1);
+                        right_half = to_parse.substr(i+2);
+                        parse_command(left_half, 0);
+                        parse_command(right_half,0);
                         break;
-            case 'c':
-                        if(to_parse[i+1] == 'd'){
+            case 'd':
+                        if(to_parse[i-1] == 'c'){
                             std::cout << "TODO implement changing directory" << std::endl;
                         }
-                        parsed = parsed + to_parse_char;
+                        parsed = to_parse_char + parsed;
+                        break;
+            case '-':
+                        if(to_parse[i-1] == ' '){
+                            std::cout << "Argument detected" << std::endl;
+                        }
+                        parsed = to_parse_char + parsed;
                         break;
             default:
-                        parsed = parsed + to_parse_char;
+                        parsed = to_parse_char + parsed;
                         break;
         }
     }
